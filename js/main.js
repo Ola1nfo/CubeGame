a3e813695f0e04327bd8a55e3e9158cd65b1cab5
Ola1nfo <t-baiunm@mon.gov.ua>
2025-04-28 12:28:35 +0300

    some change

--- js/main.js
+++ js/main.js
@@ -1,33 +1,44 @@
 document.getElementById('start').addEventListener('click', playGame);
 
 function playGame() {
+    const resultBox = document.getElementById('result');
+    resultBox.style.display = 'none';
+
     const playerRoll = rollDice('playerCube');
     const computerRoll = rollDice('computerCube');
-    const resultBox = document.getElementById('result');
-    
-    if (playerRoll > computerRoll) {
-        resultBox.style.display = 'block'
-        resultBox.textContent = `🎉 Ти виграв! (${playerRoll} > ${computerRoll})`;
-        resultBox.style.backgroundColor = '#d4edda';
-        resultBox.style.color = '#155724';
-    } else if (playerRoll < computerRoll) {
-        resultBox.style.display = 'block'
-        resultBox.textContent = `💻 Комп'ютер виграв! (${computerRoll} > ${playerRoll})`;
-        resultBox.style.backgroundColor = '#f8d7da';
-        resultBox.style.color = '#721c24';
-    } else {
-        resultBox.style.display = 'block'
-        resultBox.textContent = `🤝 Нічия! (${playerRoll} = ${computerRoll})`;
-        resultBox.style.backgroundColor = '#fff3cd';
-        resultBox.style.color = '#856404';
-    }
+
+    setTimeout(() => {
+        resultBox.style.display = 'block';
+
+        if (playerRoll > computerRoll) {
+            resultBox.textContent = `🎉 Ти виграв! (${playerRoll} > ${computerRoll})`;
+            resultBox.style.backgroundColor = '#d4edda';
+            resultBox.style.color = '#155724';
+        } else if (playerRoll < computerRoll) {
+            resultBox.textContent = `💻 Комп'ютер виграв! (${playerRoll} < ${computerRoll})`;
+            resultBox.style.backgroundColor = '#f8d7da';
+            resultBox.style.color = '#721c24';
+        } else {
+            resultBox.textContent = `🤝 Нічия! (${playerRoll} = ${computerRoll})`;
+            resultBox.style.backgroundColor = '#fff3cd';
+            resultBox.style.color = '#856404';
+        }
+    }, 1000);
 }
 
 function rollDice(cubeId) {
     const roll = Math.floor(Math.random() * 6) + 1;
-    const cube = document.getElementById(cubeId);
+  const cube = document.getElementById(cubeId);
+
+  cube.classList.remove('cube-roll');
+  void cube.offsetWidth;
+  cube.classList.add('cube-roll');
+
+  setTimeout(() => {
     cube.src = `./img/${getImageName(roll)}`;
-    return roll;
+  }, 600);
+
+  return roll;
 }
 
 function getImageName(num) {
